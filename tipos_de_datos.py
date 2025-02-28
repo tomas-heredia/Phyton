d41858508d3e19271a1bbeefd8a294373c7baf1e
LAMALEDUCADA\tomas <tomasheredia04@hotmail.com>
2025-02-28 15:50:11 -0300

    primeros pasos

---
+++ tipos_de_datos.py
@@ -0,0 +1,38 @@
+"string"
+'string'
+
+"""tus datos son:
+nombre: lucas
+apellido: dalto
+"""
+'''tus datos son:
+nombre: lucas
+apellido: dalto 
+'''
+
+40
+40.2
+
+False
+True
+
+nombre ="lucas"
+nombre = "Dalto"
+nombre = "Pedrito"
+print(nombre)
+
+numero = 10 
+numero += 1
+print(numero)
+
+nombre = "tomas"
+mensaje = "hola "+ nombre+ "."
+print(mensaje)
+
+edad = 23
+mensaje = f"yo teng: {edad} años."
+print(mensaje)
+
+#revisa si elementos eisten en un string operadires de pertenencia in y not in
+print("yo" in mensaje)
+print("yo" not in mensaje)
